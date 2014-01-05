df69acbbb47f6da431f1eb0b78c92a62e65a10f8
Paul Jovanovic <paul@stacksocial.com>
2014-01-05 13:03:41 -0800

    Cool

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ingamefame::Application.config.secret_token = '9e9f7f14ceec10d2854b77d0470bc8c84de082aa967eda1810fa80935d3ef80d01662ef3cd2ad262c30be0b8878adc88088d30b26d5547334f1832042f888bca'
